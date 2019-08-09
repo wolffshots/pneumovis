4b2d891d2a9f132f11662c1fde0afbe086f72992
jadon <jadonwolffs@gmail.com>
2019-08-09 14:04:38 +0200

    added django-dash

--- pneumovis/pneumovis/urls.py
+++ pneumovis/pneumovis/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
+    path('django_plotly_dash/', include('django_plotly_dash.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # this allows the urls to register the media files
