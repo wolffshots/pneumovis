4c06139274a814334d7fbc8f48f6b2de0ccbeaad
jadon <jadonwolffs@gmail.com>
2019-08-13 17:26:27 +0200

    added missing page titles and favicons

--- pneumovis/pneumovis/urls.py
+++ pneumovis/pneumovis/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include   # include is necessary to be added
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
