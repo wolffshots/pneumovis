5d0a6651e735c416678f989cbfd63d06cc8fa306
jadon <jadonwolffs@gmail.com>
2019-08-23 13:11:10 +0200

    added SwabAdmin class to represent swab in admin area

--- pneumovis/swabs/admin.py
+++ pneumovis/swabs/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Swab
+
+
+class SwabAdmin(admin.ModelAdmin):
+    list_display = ('Particcipant_ID', 'Barcode')
+    list_display_links = ('Barcode',)
+    search_fields = ('Particcipant_ID', 'Barcode')
+
+
+admin.site.register(Swab, SwabAdmin)
