fa08b5a62580f6e7d338a975a5d9fc62a44a8e90
jadon <jadonwolffs@gmail.com>
2019-08-24 01:23:29 +0200

    added paths to 'participants' and specific particpants

---
+++ pneumovis/participants/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from swabs.models import Swab
+from django.db.models import Count
+
+participants = Swab.objects.order_by('Particcipant_ID').values('Particcipant_ID').annotate(
+    dcount=Count('Particcipant_ID'))
+urlpatterns = [
+    path('', views.index, name='participants'),
+    path('<slug:Particcipant_ID>', views.participant, name='participant'),
+]
--- pneumovis/pneumovis/urls.py
+++ pneumovis/pneumovis/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('swabs/', include('swabs.urls')),
+    path('participants/', include('participants.urls')),
     path('django_plotly_dash/', include('django_plotly_dash.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # this allows the urls to register the media files
