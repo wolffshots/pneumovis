1b163bb4e17bd9c43e90c5bc792f72fd602de74e
jadon <jadonwolffs@gmail.com>
2019-08-24 02:41:12 +0200

    added browse page

--- pneumovis/pages/urls.py
+++ pneumovis/pages/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'), # directs user to index when no path
-    path('about', views.about, name='about'), # directs user to about page when /about
+    path('', views.index, name='index'),  # directs user to index when no path
+    # directs user to about page when /about
+    path('about', views.about, name='about'),
     path('logout', views.logout, name='logout'),
     path('dashboard', views.dashboard, name='dashboard'),
     path('test', views.dep, name='test'),
@@ -11,4 +12,5 @@ urlpatterns = [
     path('dep', views.dep, name='register'),
     path('login', views.login, name='login'),
     path('ip/', views.ip, name='ip'),
+    path('browse', views.browse, name='browse'),
 ]
--- pneumovis/pages/views.py
+++ pneumovis/pages/views.py
@@ -1,113 +1,28 @@
 from django.http import HttpResponse
 # temporary such that we can server hello world
 from django.shortcuts import render, redirect
-    
+
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
-from .dash_app import * # this imports the app from a separate file (just for easier editing and modularity)
+# this imports the app from a separate file (just for easier editing and modularity)
+from .dash_app import *
+
 
 def index(request):
-    # listings = Listing.objects.order_by(
-    #     '-list_date').filter(published=True)[:3]
-
-    # context = {
-    #     'listings': listings,
-    #     'state_choices': state_choices,
-    #     'bedroom_choices': bedroom_choices,
-    #     'price_choices': price_choices
-    # }
-
-    #return HttpResponse('<h1>Hello world</h1>')# we want to actually render a template but html can go here
-    # takes in the request and the location of the template
-    # return render(request, 'pages/index.html', context)
-    # messages.error(request, 'Test?')
     return render(request, 'pages/index.html')
 
 
 def about(request):
-    # realtors = Realtor.objects.order_by('-hire_date')
-    # mvps = Realtor.objects.all().filter(mvp=True)
-
-    # context = {
-    #     'realtors': realtors,
-    #     'mvps': mvps
-    # }
-    # return render(request, 'pages/about.html', context)
     return render(request, 'pages/about.html')
 
-
-# def logout(request):
-#     if request.method == 'POST':
-#         auth.logout(request)
-#         messages.success(request, 'Successfully logged out')
-#         return redirect('index')
-
-# def login(request):
-#     # if request.method == 'POST':
-#         # auth.logout(request)
-#         # messages.success(request, 'Successfully logged out')
-#     return render(request, 'pages/login.html')
 
 def dashboard(request):
     return redirect('dashboard')
 
+
 def dep(request):
     messages.error(request, 'Not yet implemented')
     return redirect('index')
-
-
-# def register(request):
-#     if request.method == 'POST':
-#         # form submiusion
-#         # print("submitted reg")
-#         # messages.error(request, 'testing error message')
-#         # return redirect('register')
-#         # register logic
-
-#         # Capture values
-#         first_name = request.POST['first_name']
-#         last_name = request.POST['last_name']
-#         username = request.POST['username']
-#         email = request.POST['email']
-#         password = request.POST['password']
-#         password2 = request.POST['password2']
-
-#         # check passwords
-#         if password == password2:
-#             # Check username
-#             if User.objects.filter(username=username).exists():
-#                 messages.error(request, 'Username is already taken')
-#                 return redirect('register')
-#             else:
-#                 if User.objects.filter(email=email).exists():
-#                     messages.error(request, 'Email is already in use')
-#                     return redirect('register')
-#                 else:
-#                     # Looks good
-#                     # return redirect('register')
-#                     # pass
-#                     user = User.objects.create_user(
-#                         username=username, password=password, email=email, first_name=first_name, last_name=last_name)
-#                     # passwords are automagically hashed
-#                     # Login after register
-
-#                     ## Method 1 is to log straight in
-#                     # auth.login(request,user)
-#                     # messages.success(request,'You are now logged in')
-#                     # return redirect('register')
-
-#                     ## Method 2 is to save and redirect to log in
-#                     user.save()
-#                     messages.success(
-#                         request, 'You are now registered, please log in')
-#                     return redirect('login')
-#         else:
-#             messages.error(request, 'Passwords do not match')
-#             return redirect('register')
-#         pass
-#     else:
-#         return render(request, 'accounts/register.html')
-#         return redirect('register')
 
 
 def login(request):
@@ -133,5 +48,10 @@ def logout(request):
         messages.success(request, 'Successfully logged out')
         return redirect('index')
 
+
 def ip(request):
     return render(request, 'pages/index-proto.html')
+
+
+def browse(request):
+    return render(request, 'pages/browse.html')
